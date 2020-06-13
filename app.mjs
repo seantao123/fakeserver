972d02a41f698a0cd3a85fba61d10b9a7e3abcfa
wtao <wtao@redfastlabs.com>
2020-06-13 14:39:57 -0700

    sean pukes and sucks

--- app.mjs
+++ app.mjs
@@ -7,7 +7,7 @@ const server = app.listen(port);
 app.get("/api/location/search", (request, response) => {
   response.json([
         {
-            "title": "San Jose",
+            "title": "sean pukie",
             "location_type": "City",
             "woeid": 2488042,
             "latt_long": "37.338581,-121.885567"
